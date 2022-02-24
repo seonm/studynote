f164b484f23470c75c83b4d2a6704d3ea93b3fe7
seonm <misoda1004@gmail.com>
2022-02-24 16:22:31 +0900

    html

--- 02_javascript/05-기본문법-활용하기/연습문제test.js
+++ 02_javascript/05-기본문법-활용하기/연습문제test.js
@@ -1,8 +1,21 @@
-function solution(x) {
-  var answer = true;
-  
-  x.toString();
-  
+function solution(num) {
+  var answer = 0;
+  for(i=0; i<500; i++){
+    if (num == 1){
+      break;
+    }else if(!num == 1){
+      continue;
+    }
+    if(num % 2 === 0){
+      num = num / 2
+    }else{
+      num = num * 3 + 1
+    }
+    answer++
+  }
+  if(answer >= 500){
+      return answer = -1
+  }
   return answer;
 }
-console.log(solution(28))
+console.log(solution(626331))
