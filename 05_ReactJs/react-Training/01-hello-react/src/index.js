351d6a3ebaa18baa1c42f394facf547ce876b29d
seonm <misoda1004@gmail.com>
2022-04-25 15:32:57 +0900

    react exam01

--- 05_ReactJs/react-Training/01-hello-react/src/index.js
+++ 05_ReactJs/react-Training/01-hello-react/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-import App2 from './question/App2'; // 연습문제
+import App from './App'; // 연습문제
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>
 );
